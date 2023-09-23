08ca1ea6e2e4d71214ca99a81fff18fc53129423
NazarovIlya <eliakohai@gmail.com>
2023-09-24 00:37:39 +0300

    Navigation Bar complited

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,22 @@
 <template>
+  <navigation-bar type="header"/>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
+
+<script>
+import NavigationBar from './components/base/NavigationBar.vue';
+
+export default ({
+  name: 'App',
+  components: {
+    NavigationBar,
+  },
+});
+</script>
 
 <style lang="scss">
 #app {
